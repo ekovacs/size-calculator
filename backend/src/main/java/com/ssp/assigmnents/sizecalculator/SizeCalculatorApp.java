3b20801d5a9b7de5ace1b8ef51960cb739895bff
Endre Zoltan Kovacs <ekovacs@b2international.com>
2017-06-25 23:05:59 +0200

    spring boot app runs again :)

--- backend/src/main/java/com/ssp/assigmnents/sizecalculator/SizeCalculatorApp.java
+++ backend/src/main/java/com/ssp/assigmnents/sizecalculator/SizeCalculatorApp.java
@@ -3,13 +3,11 @@ package com.ssp.assigmnents.sizecalculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ssp.assigmnents.sizecalculator.rest.SizeCalculatorRestService;
-
 @SpringBootApplication
 public class SizeCalculatorApp {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(SizeCalculatorRestService.class, args);
+		SpringApplication.run(SizeCalculatorApp.class, args);
 	}
 
 }
