c39604bff5e391fea295a1ef6a25e3d56d27000e
Endre Zoltan Kovacs <ekovacs@b2international.com>
2017-06-25 17:05:10 +0200

    equals and hashcode added for domain objects

--- backend/src/main/java/com/ssp/assigmnents/domain/Brand.java
+++ backend/src/main/java/com/ssp/assigmnents/domain/Brand.java
@@ -2,24 +2,55 @@ package com.ssp.assigmnents.domain;
 
 public class Brand {
 	
-	private String key;
+	private final String key;
 	
-	private String name;
+	private final String name;
 
 	public String getKey() {
 		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	private Brand(String key, String name) {
+		this.key = key;
 		this.name = name;
 	}
 	
+	public static Brand of(String key, String name) {
+		return new Brand(key, name);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Brand other = (Brand) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
--- backend/src/main/java/com/ssp/assigmnents/domain/Category.java
+++ backend/src/main/java/com/ssp/assigmnents/domain/Category.java
@@ -4,35 +4,69 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Category {
 	
-	private String key;
+	private final String key;
 	
-	private String name;
+	private final String name;
 	
 	@JsonProperty("measurement_type")
-	private String measurementType;
+	private final String measurementType;
 
 	public String getKey() {
 		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getMeasurementType() {
 		return measurementType;
 	}
-
-	public void setMeasurementType(String measurementType) {
+	
+	private Category(String key, String name, String measurementType) {
+		this.key = key;
+		this.name = name;
 		this.measurementType = measurementType;
 	}
 	
+	public static Category of(String key, String name, String measurementType) {
+		return new Category(key, name, measurementType);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((measurementType == null) ? 0 : measurementType.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (measurementType == null) {
+			if (other.measurementType != null)
+				return false;
+		} else if (!measurementType.equals(other.measurementType))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
