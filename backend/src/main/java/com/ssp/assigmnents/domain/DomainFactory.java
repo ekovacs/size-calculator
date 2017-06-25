c2733a682cca208ded05a9edc88759ba29b17a57
Endre Zoltan Kovacs <ekovacs@b2international.com>
2017-06-25 17:00:04 +0200

    a place to instantiate / wire objects

---
+++ backend/src/main/java/com/ssp/assigmnents/domain/DomainFactory.java
@@ -0,0 +1,58 @@
+package com.ssp.assigmnents.domain;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+
+public class DomainFactory {
+	
+	private static Multimap<Brand, Category> brandToCategoryMapping = 
+			ImmutableMultimap.<Brand, Category> builder()
+				
+			.putAll(Brand.of("calvin-klein", "Calvin Klein"), 
+							Category.of("dresses", "Dresses", "bust"), 
+							Category.of("jeans", "Jeans", "waist"))
+							// c-k doesn't sell shoes
+				.putAll(Brand.of("diane-von-furstenberg", "Diane von Furstenberg"),
+							Category.of("dresses", "Dresses", "bust"), 
+							Category.of("jeans", "Jeans", "waist"),
+							Category.of("sneakers", "Sneakers", "length"))
+				
+				.putAll(Brand.of("michael-kors", "Michael Kors"), 
+							Category.of("dresses", "Dresses", "bust"), 
+							Category.of("jeans", "Jeans", "waist"),
+							Category.of("sneakers", "Sneakers", "length"))
+			.build();
+	
+	
+	public static Brand brandForKey(String key) {
+		return getBrands().stream()
+				.filter(brand -> brand.getKey().equals(key))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException(String.format("Unknown brand used as key: %s", key)));
+	}
+	
+	
+	public static Category categoryForKey(String key) {
+		return brandToCategoryMapping.values().stream()
+				.filter(category -> category.getKey().equals(key))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException(String.format("Unknown category used as key: %s", key)));
+	}
+	
+	public static Collection<Brand> getBrands() {
+		return brandToCategoryMapping.keySet();
+	}
+
+	public static Collection<Category> getCategoriesFor(String brand) {
+		return brandToCategoryMapping.asMap().entrySet().stream()
+				.filter(entry -> entry.getKey().getKey().equals(brand))
+				.flatMap(entry -> entry.getValue().stream())
+				.collect(Collectors.toSet());
+	}
+	
+	
+	
+}
